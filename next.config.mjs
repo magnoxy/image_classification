5fa5b291fb7b8b6c1c55f94b674daca87ee0cdf2
Edson Magno <89936874+magnoxy@users.noreply.github.com>
2024-06-10 22:58:04 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["img.freepik.com", "static.vecteezy.com", "github.com", "i.redd.it"],
+  },
+};
+
+export default nextConfig;
